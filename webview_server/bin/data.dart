4344e5bea727736aafa39cef1e9f5b837d3a3e4d
Aswin Gopinathan <40236624+infiniteoverflow@users.noreply.github.com>
2022-09-17 06:06:45 +0530

    Added server files

---
+++ webview_server/bin/data.dart
@@ -0,0 +1,10 @@
+class Data {
+  static Map<String, dynamic> webviewData = {
+    'data': {
+      '1': 'https://flutter.dev',
+      '2': 'https://dart.dev',
+      '3':
+          'https://gdg.community.dev/events/details/google-gdg-bangalore-presents-devfest-bangalore-2022/',
+    },
+  };
+}
